0e978f84ca6fe9d3a2c12b7e37be3f2c81015714
davhyandrade <davhyhello@gmail.com>
2026-09-20 15:52:09 -0300

    chore: adds storybook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig, globalIgnores } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
 import prettierRecommendedConfig from 'eslint-plugin-prettier/recommended';
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from 'eslint-plugin-storybook';
 
 import generalConfig from './eslint/general.config.mjs';
 import importSortConfig from './eslint/import.config.mjs';
@@ -9,6 +11,7 @@ import importSortConfig from './eslint/import.config.mjs';
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  ...storybook.configs['flat/recommended'],
   prettierRecommendedConfig,
   generalConfig,
   importSortConfig,
